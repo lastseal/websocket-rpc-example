32f3cef723ff54ac1e5ceeefa8637dfa62f30bc4
phuenchullan-lastseal <87383650+phuenchullan-lastseal@users.noreply.github.com>
2023-05-22 12:19:41 -0400

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,1 +1,18 @@
 
+
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({port:8081}, ()=>{
+    console.log("Server started");
+});
+
+wss.on('connection', (ws) => {
+    console.log("connected....");
+    ws.on('message', (data) => {
+        console.log("data received "+data);
+    });
+});
+
+wss.on('listening', ()=> {
+    console.log("server is listening on port 8081");
+});
