f32b7fd338ff873252ad22183b4309895e425ca1
phuenchullan-lastseal <87383650+phuenchullan-lastseal@users.noreply.github.com>
2023-05-22 12:20:15 -0400

    Update controller.cs

--- unity/controller.cs
+++ unity/controller.cs
@@ -1,1 +1,15 @@
+using WebSocketSharp;
 
+namespace myfirstapp{
+    public class Program {
+        static void Main(string[] args){
+            WebSocket ws = new WebSocket("ws://localhost:8081");
+            ws.OnMessage += (sender, e) =>
+            Console.WriteLine ("Laputa says: " + e.Data);
+
+            ws.Connect();
+            ws.Send ("BALUS");
+        }
+    }
+
+}
